9d78f80bb3f3b32d5f6b088efbbce93282e015d8
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-13 14:54:53 -0500

    Delete weekly_mission_2/examples/weekly_mission_2/exercises/examples/examples5 directory
    5

--- weekly_mission_2/examples/weekly_mission_2/exercises/examples/examples5/pokemon.test.js
+++
@@ -1,7 +0,0 @@
-import Pokemon from './pokemon'
-
-test('1) Create a new object pokemon', () => {
-  const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
-});
-
