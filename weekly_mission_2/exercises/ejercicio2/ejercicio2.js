5bca95eb417889102a71932ebb5be6ba05cc54a6
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-17 13:23:12 -0500

    update titulos ejercicio 2

--- weekly_mission_2/exercises/ejercicio2/ejercicio2.js
+++ weekly_mission_2/exercises/ejercicio2/ejercicio2.js
@@ -59,32 +59,32 @@ const explorers = [
     }  
    ]
  
-   console.log(".....");
+   console.log("Ejercicio 1.2");
    explorers.forEach(nombre => console.log('Nombre Explorer: '+ nombre.name));
-   console.log(".....");
+   console.log("ejercicio 2.2");
    explorers.forEach(nombre => console.log('Stack: '+ nombre.stack)); 
-   console.log(".....");
+   console.log("ejercicio 3.2");
    const listastack = explorers.map((sta) => sta.stack);
    console.log(listastack);
    
-   console.log(".....");
+   console.log("ejercicio 4.2");
    const js = explorers.filter((lis) => lis.stack.includes('js'));
    console.log(js);
 
-   console.log(".....");
+   console.log("ejercicio 5.2");
    const cdmx = explorers.find((cdmx) => cdmx.city === 'CDMX');
   console.log("El primer explorer que es de la cdmx es: " + cdmx.name);
   
-  console.log(".....");
+  console.log("ejercicio 6.2");
   console.log("Suma de exercises_completed: ");
   const suma = explorers.reduce ((acumulador, valoractual) =>  acumulador + valoractual.exercises_completed, 0);
   console.log(suma);
   
-  console.log(".....");
+  console.log("ejercicio 7.2");
   const yesTrueSome = explorers.some((t)=> t.exercisesFinished || true);
   console.log("Al menos uno de los explorer tiene la propiedad exercisesFinished ?: "  + yesTrueSome);
   
-  console.log(".....");
+  console.log("ejercicio 8.2");
   const yesTrueEvery = explorers.every((name) => name.isFinished || true);
   console.log("Al menos uno de los explorer tiene la propiedad isFinished?: " + yesTrueEvery);
   
